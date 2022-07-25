9c843cbc3b55f0ee0caa148dd37b1fe4f4d2899f
bonnw <pwonghansa@gmail.com>
2022-07-25 00:37:06 -0500

    created test slash commands

--- dnd.js
+++ dnd.js
@@ -1,10 +1,24 @@
-const { Client } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
-const client = new Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once("ready", () => {
   console.log("bot live");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
+  }
 });
 
 client.login(process.env.TOKEN);
